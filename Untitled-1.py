f763dda8304847f43277295112b816f2ef7a1cb7
ValterCarlens <valter.carlens@elev.ga.ntig.se>
2023-08-24 13:14:15 +0200

    förstakod

---
+++ Untitled-1.py
@@ -0,0 +1,26 @@
+print("Hello world")
+print("kungen av \"nacka\" \n hepp\twie")
+print("Robert" + " " + "Carlens")
+print("Fy " + "17!")
+
+# Här läser vi in input och skriver ut svaret
+print("Vad heter du?")
+svar=input()
+print("Hej", svar)
+
+
+print("""Lorem Ipsu
+Lorem Ipsu 
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu
+Lorem Ipsu""")
+
+i=1
+while i < 10:
+      print("Lorem Ipsu")
+      i += 1
